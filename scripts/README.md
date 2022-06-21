bc0bc670e94cc681c0595647dd020ded8305b487
Harshith Mohan Kumar <hiharshith18@gmail.com>
2022-06-21 10:37:00 +0530

    BASH SCRIPT WORKSgit add .!

---
+++ scripts/README.md
@@ -0,0 +1,31 @@
+
+
+To cancel all queued jobs:
+```
+scancel -u $USER
+```
+
+To see job status:
+```
+squeue -u $USER 
+# or
+sacct -u $USER
+```
+
+To see general info about nodes:
+```
+sinfo
+```
+
+Requesting GPU (example):
+```
+srun -p gpu -C gpu2v100 --gres=gpu:1 --pty bash
+```
+
+```
+cd gsoc
+
+lsyncd -nodaemon -rsyncssh ./ hxm471@rider.case.edu /mnt/rds/redhen/gallina/home/hxm471/RedHenLab-Multimodal_TV_Show_Segmentation
+```
+
+rsync -az hpc3:/mnt/rds/redhen/gallina/home/hxm471/RedHenLab-Multimodal_TV_Show_Segmentation/scripts /tmp/$USER/
--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo Hello World!
+# Makes the bash script to print out every command before it is executed except echo
+trap '[[ $BASH_COMMAND != echo* ]] && echo $BASH_COMMAND' DEBUG
 
 n=26
 i=0
 allFiles=()
 while IFS= read -r line; do
-	if [ $i == $n ] 
+	echo $line
+	echo $i
+	echo $n
+	if [ $i -eq $n ] 
 	then
+		echo i equal to n
 		echo "$line"
 		allFiles+=($line)
 		
 		for f in ${allFiles[@]}; do
 			echo $f
-			rsync -az hpc3:$line /tmp/$USER/${f} /tmp/$USER/mtvss/data/tmp/video_files
+			rsync -az hpc3:${f} /tmp/$USER/mtvss/data/tmp/video_files
 		done
 	fi
 	i=$((i+1))
