fb25bef43bc4a7842f0d906cdfbd0a22d0490c7c
Harshith Mohan Kumar <hiharshith18@gmail.com>
2022-07-05 12:19:46 +0530

    Excluded rsync

--- scripts/test_script.sh
+++ scripts/test_script.sh
@@ -21,22 +21,22 @@ cd /tmp/$USER/
 mkdir /tmp/$USER/video_files
 
 # Write script to read batch of Rosenthal files based on Array Job Index 0
-n=0
-i=0
-allFiles=()
-while IFS= read -r line; do
-	if [ $i -eq $n ] 
-	then
-		echo i equal to n
-		allFiles+=($line)
+# n=0
+# i=0
+# allFiles=()
+# while IFS= read -r line; do
+# 	if [ $i -eq $n ] 
+# 	then
+# 		echo i equal to n
+# 		allFiles+=($line)
 		
-		for f in ${allFiles[@]}; do
-			echo $f
-			rsync -az hpc3:${f} /tmp/$USER/video_files
-		done
-	fi
-	i=$((i+1))
-done < /tmp/$USER/mtvss/data/tmp/batch_cat1.txt
+# 		for f in ${allFiles[@]}; do
+# 			echo $f
+# 			rsync -az hpc3:${f} /tmp/$USER/video_files
+# 		done
+# 	fi
+# 	i=$((i+1))
+# done < /tmp/$USER/mtvss/data/tmp/batch_cat1.txt
 
 # Run singularity container -- Pipeline Stage 1 -- Music Classification
 singularity exec -e --nv -B /tmp/$USER/ /scratch/users/$USER/mtvss_dev2.sif python3 /tmp/$USER/mtvss/pipeline_stage1/run_pipeline_stage1.py --job_num=0 --model="music" --verbose=True
